f0ad965d789328e9f8a7d35eda302bb036b53201
jeldikk <jeldi.kamal2013@gmail.com>
2024-12-07 17:23:20 +0530

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("should have 'i am modified' text", () => {
+  render(<App />);
+  const element = screen.getByText(/i am modified/i);
+  expect(element).toBeInTheDocument();
+});
+
+test("docker should detect this test", () => {
+  render(<App />);
+  const element = screen.getByText(/extra test/i);
+  expect(element).toBeInTheDocument()
+});
+
+test("should have a header with class App-header", () => {
+  const {container} = render(<App />);
+  const header = container.querySelector('header');
+  expect(header).toHaveClass('App-header')
+})
